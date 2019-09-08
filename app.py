3c7f9446e2ce760c56dd98aabde281eaa84f9433
Debashis <debmallik.29@gmail.com>
2019-09-08 20:53:29 +0530

    first use of template

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
-from flask import Flask
+from flask import Flask,render_template
 app=Flask(__name__)
 
-@app.route('/')
-def index():
-    return '<h1> Hello World !!! </h1>'
+posts=[
+    {
+        "author":"Debashis",
+        "title":"Blog post 1",
+        "blog":"Proud to be Indian",
+        "date":"8th Sept 2019"
+    },
+    {
+        "author":"Mallik",
+        "title":"Blog post 2",
+        "blog":"I stay in Bangalore",
+        "date":"8th Sept 2019"
+    }
+]
+
+@app.route('/home')
+def home():
+    return render_template('home.html',posts=posts)
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html',posts=posts)
 
 if __name__ == "__main__":
     app.run(debug=True)
