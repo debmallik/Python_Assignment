2abc68455d3da903c7c3e74f7e9fd9950a526c73
Debashis <debmallik.29@gmail.com>
2019-09-10 02:33:31 +0530

    base web page

--- app.py
+++ app.py
@@ -1,28 +1,24 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,url_for
 app=Flask(__name__)
 
 posts=[
     {
-        "author":"Debashis",
-        "title":"Blog post 1   ",
-        "blog":"Proud to be Indian",
-        "date":"8th Sept 2019"
-    },
-    {
-        "author":"Mallik",
-        "title":"Blog post 2",
-        "blog":"I stay in Bangalore",
-        "date":"8th Sept 2019"
+        "message":"Welcome to GROCESTORE",
+        "promise":"Our Guarantee:",
+        "point1":"Online Delivery",
+        "point2":"Free Delivery",
+        "point3":"Best Quality",
     }
+
 ]
 
 @app.route('/home')
 def home():
-    return render_template('home.html',posts=posts)
+    return render_template('home.html',posts=posts,title='home')
 
-@app.route('/profile')
-def profile():
-    return render_template('profile.html',title='profile')
+#@app.route('/profile')
+#def profile():
+ #   return render_template('profile.html',title='profile')
 
 if __name__ == "__main__":
     app.run(debug=True)
